c9c2ef90aa2cb6a5c719c043d69c41dfaeb9eb73
Na7aly <tomac.natalia95@gmail.com>
2024-07-24 22:52:38 +0300

    .

--- src/components/ContactPage/ContactPage.jsx
+++ src/components/ContactPage/ContactPage.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import styles from './ContactPage.module.css'; // Asigură-te că stilurile sunt corecte
+import styles from './ContactPage.module.css'; // Importă stilurile module
 
 const ContactPage = () => {
   const [formData, setFormData] = useState({
@@ -41,117 +41,108 @@ const ContactPage = () => {
   };
 
   return (
-    
     <section id="contact" className={styles.container}>
-        <div className="text-center">
-            <h2 className="section-heading">Contactați-ne</h2>
-            <h3 className="section-subheading text-muted">Completați formularul pentru a ne contacta.</h3>
-          </div>
+      <div className={styles.textCenter}>
+        <h2 className={styles.sectionHeading}>Contactați-ne</h2>
+        <h3 className={styles.sectionSubheading}>Completați formularul pentru a ne contacta.</h3>
+      </div>
 
-        <div className={styles.contactContainer}>
-        
-      <div className={styles.contactWrapper}>
-        
-        <div className={styles.contactInfo}>
-          <div className={styles.infoItem}>
-            <i className="fas fa-map-marker-alt"></i>
-            <span>ADRESA</span>
-            <p>Strada Alexandr Puskin 19, etajul 3, Bălți, Moldova</p>
-          </div>
-          <div className={styles.infoItem}>
-            <i className="fas fa-phone"></i>
-            <span>TELEFON</span>
-            <p>+373 (60) 229996 - Constantin Curca</p>
-            <p>+373 (79) 971786 - Ioan-Giuliano Ciomschi</p>
-          </div>
-          <div className={styles.infoItem}>
-            <i className="fas fa-envelope"></i>
-            <span>EMAIL</span>
-            <p>youth.center@studem.md</p>
-            <p>studem.balti@gmail.com</p>
-          </div>
-          <div className={styles.infoItem}>
-            <i className="fas fa-clock"></i>
-            <span>ORELE DE LUCRU</span>
-            <p>Luni - Duminică: 10.00-22.00</p>
-          </div>
-        </div>
-
-        <div className={styles.contactForm}>
-          {/* <div className="text-center">
-            <h2 className="section-heading">Contactați-ne</h2>
-            <h3 className="section-subheading text-muted">Completați formularul pentru a ne contacta.</h3>
-          </div> */}
-          <form id="contactForm" onSubmit={handleSubmit}>
-            <div className="row">
-              <div className="col-md-6">
-                <div className="form-group">
-                  <input
-                    type="text"
-                    className="form-control"
-                    placeholder="Numele"
-                    id="name"
-                    required
-                    value={formData.name}
-                    onChange={handleInputChange}
-                  />
-                </div>
-                <div className="form-group">
-                  <input
-                    type="email"
-                    className="form-control"
-                    placeholder="Email"
-                    id="email"
-                    required
-                    value={formData.email}
-                    onChange={handleInputChange}
-                  />
-                </div>
-                <div className="form-group">
-                  <input
-                    type="text"
-                    className="form-control"
-                    placeholder="Subiectul"
-                    id="subject"
-                    required
-                    value={formData.subject}
-                    onChange={handleInputChange}
-                  />
-                </div>
-                <div className="form-group">
-                  <input
-                    type="text"
-                    className="form-control"
-                    placeholder="Numărul de telefon"
-                    id="phone"
-                    required
-                    value={formData.phone}
-                    onChange={handleInputChange}
-                  />
-                </div>
-              </div>
-              <div className="col-md-6">
-                <div className="form-group">
-                  <textarea
-                    className="form-control"
-                    placeholder="Mesajul"
-                    id="message"
-                    required
-                    value={formData.message}
-                    onChange={handleInputChange}
-                  ></textarea>
-                </div>
-              </div>
-              <div className="clearfix"></div>
-              <div className="text-center">
-                <button type="submit" className="btn btn-xl">Trimite mesaj</button>
-              </div>
+      <div className={styles.contactContainer}>
+        <div className={styles.contactWrapper}>
+          <div className={styles.contactInfo}>
+            <div className={styles.infoItem}>
+              <i className={`${styles.icon} fas fa-map-marker-alt`}></i>
+              <span>ADRESA</span>
+              <p>Strada Alexandr Puskin 19, etajul 3, Bălți, Moldova</p>
             </div>
-          </form>
+            <div className={styles.infoItem}>
+              <i className={`${styles.icon} fas fa-phone`}></i>
+              <span>TELEFON</span>
+              <p>+373 (60) 229996 - Constantin Curca</p>
+              <p>+373 (79) 971786 - Ioan-Giuliano Ciomschi</p>
+            </div>
+            <div className={styles.infoItem}>
+              <i className={`${styles.icon} fas fa-envelope`}></i>
+              <span>EMAIL</span>
+              <p>youth.center@studem.md</p>
+              <p>studem.balti@gmail.com</p>
+            </div>
+            <div className={styles.infoItem}>
+              <i className={`${styles.icon} fas fa-clock`}></i>
+              <span>ORELE DE LUCRU</span>
+              <p>Luni - Duminică: 10.00-22.00</p>
+            </div>
+          </div>
+
+          <div className={styles.contactForm}>
+            <form id="contactForm" onSubmit={handleSubmit}>
+              <div className={styles.formRow}>
+                <div className={styles.formColumn}>
+                  <div className={styles.formGroup}>
+                    <input
+                      type="text"
+                      className={styles.formControl}
+                      placeholder="Numele"
+                      id="name"
+                      required
+                      value={formData.name}
+                      onChange={handleInputChange}
+                    />
+                  </div>
+                  <div className={styles.formGroup}>
+                    <input
+                      type="email"
+                      className={styles.formControl}
+                      placeholder="Email"
+                      id="email"
+                      required
+                      value={formData.email}
+                      onChange={handleInputChange}
+                    />
+                  </div>
+                  <div className={styles.formGroup}>
+                    <input
+                      type="text"
+                      className={styles.formControl}
+                      placeholder="Subiectul"
+                      id="subject"
+                      required
+                      value={formData.subject}
+                      onChange={handleInputChange}
+                    />
+                  </div>
+                  <div className={styles.formGroup}>
+                    <input
+                      type="text"
+                      className={styles.formControl}
+                      placeholder="Numărul de telefon"
+                      id="phone"
+                      required
+                      value={formData.phone}
+                      onChange={handleInputChange}
+                    />
+                  </div>
+                </div>
+                <div className={styles.formColumn}>
+                  <div className={styles.formGroup}>
+                    <textarea
+                      className={styles.formControl}
+                      placeholder="Mesajul"
+                      id="message"
+                      required
+                      value={formData.message}
+                      onChange={handleInputChange}
+                    ></textarea>
+                  </div>
+                </div>
+              </div>
+              <div className={styles.textCenter}>
+                <button type="submit" className={styles.submitButton}>Trimite mesaj</button>
+              </div>
+            </form>
+          </div>
         </div>
       </div>
-        </div>
-       
     </section>
   );
 };
