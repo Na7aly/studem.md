d51caf279a535bf47bf7c69f879f6f7a0feca950
Na7aly <tomac.natalia95@gmail.com>
2024-07-29 22:51:58 +0300

    //

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -8,7 +8,7 @@ import About from '../components/About';
 // import ContactUs from '../components/ContactUs';
 import Services from '../components/Services';
 import Testimonial from '../components/Testimonial/Testimonial';
-import Newsletter from '../components/Newsletter/Newsletter';
+// import Newsletter from '../components/Newsletter/Newsletter';
 // import ContactsDetail from '../components/ContactsDetail/Contacts';
 import NewsSection from '../components/News/News';
 import Space from 'components/Space/Space';
