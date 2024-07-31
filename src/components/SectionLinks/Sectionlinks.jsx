69ec88460942f1ee9ef9877c27a1ce7eb3c7cff5
Na7aly <tomac.natalia95@gmail.com>
2024-08-01 00:12:21 +0300

    ./

--- src/components/SectionLinks/Sectionlinks.jsx
+++ src/components/SectionLinks/Sectionlinks.jsx
@@ -21,7 +21,6 @@ const SectionLinks = () => {
           </div>
           <div className={styles.column}>
             <h3>CONTACTE</h3>
-            <address>
             <div className={styles.contactWrapper}>
           <div className={styles.contactInfo}>
             <div className={styles.infoItem}>
@@ -52,7 +51,39 @@ const SectionLinks = () => {
             </div>
           </div>
         </div>
-            </address>
+            
+            {/* <address>
+            <div className={styles.contactWrapper}>
+          <div className={styles.contactInfo}>
+            <div className={styles.infoItem}>
+              <i className={`${styles.icon} fas fa-map-marker-alt`}></i>
+              <span>ADRESA</span>
+              <p>Strada Alexandr Puskin 19, etajul 3, Bălți, Moldova</p>
+            </div>
+            <div className={styles.infoItem}>
+              <i className={`${styles.icon} fas fa-phone`}></i>
+              <span>TELEFON</span>
+              <div>
+                <p>+373 (60) 229996 - Constantin Curca</p>
+                <p>+373 (79) 971786 - Ioan-Giuliano Ciomschi</p>
+              </div>
+            </div>
+            <div className={styles.infoItem}>
+              <i className={`${styles.icon} fas fa-envelope`}></i>
+              <span>EMAIL</span>
+              <div>
+                <p>youth.center@studem.md</p>
+                <p>studem.balti@gmail.com</p>
+              </div>
+            </div>
+            <div className={styles.infoItem}>
+              <i className={`${styles.icon} fas fa-clock`}></i>
+              <span>ORELE DE LUCRU</span>
+              <p>Luni - Duminică: 10.00-22.00</p>
+            </div>
+          </div>
+        </div>
+            </address> */}
           </div>
           <div className={styles.column}>
             <div><h3>Despre noi</h3>
