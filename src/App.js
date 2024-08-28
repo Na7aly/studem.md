3f90eb3c2c6f91858a8dc939f4a00fc8c16f426f
Na7aly <tomac.natalia95@gmail.com>
2024-08-28 17:18:18 +0300

    ./

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ function App() {
     <Router>
       <Navbar />
       <Routes>
-      <Route path="" element={<HomePage />} />
+      <Route path="/studem.md/" element={<HomePage />} />
         <Route path="/" element={<HomePage />} />
         <Route path="/contact" element={<ContactPage />} />
         <Route path="/granturi" element={<GrantApplication />} />
