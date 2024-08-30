30aa86502ca3396e2cf784c7a6d35e145e1a5afd
Na7aly <tomac.natalia95@gmail.com>
2024-08-31 00:01:10 +0300

    ,/

--- src/components/contentful/contentfulClient.js
+++ src/components/contentful/contentfulClient.js
@@ -1,9 +1,8 @@
-import { createClient }  from 'contentful';
+import { createClient } from 'contentful';
 
 const client = createClient({
-  space: process.env.REACT_APP_CONTENTFUL_SPACE_ID,
-  accessToken: process.env.REACT_APP_CONTENTFUL_ACCESS_TOKEN
+  space: 'xpckm8g47k10', // Înlocuiește cu ID-ul tău de spațiu Contentful
+  accessToken: 'jCvBFbNBstia0e9P_WZjKS7ttNvEXbA3Cw3TbruN6UY' // Înlocuiește cu token-ul tău de acces Contentful
 });
-
 
 export default client;
